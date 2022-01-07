134a44b10fa46ac8fd3550d051c7301d4d47ee71
Danile <erheng07@gmail.com>
2022-01-07 12:19:59 +0800

    add switch data source

--- src/Config.js
+++ src/Config.js
@@ -1,18 +1,19 @@
 const Config = {
-  rootAPIURL: "https://api.aresprotocol.io/api",
-  auth: "/auth",
-  setWeight: "/setWeight",
-  getAresAll: "/getAresAll",
-  getPartyPrice: "/getPartyPrice", //btcusdt
-  getBulkCurrencyPrices: "/getBulkCurrencyPrices", // symbol=btc_eth_dot_link&currency=usdt
-  getReqConfig: "/getReqConfig",
-  getRequestInfoBySymbol: "/getRequestInfoBySymbol", // index=0&symbol=axsusdt
-  getHistoryPrices: "/getUpdatePriceHistory",
-  getPriceAll: "/getPriceAll",
-  getUpdatePriceHeartbeat: "/getUpdatePriceHeartbeat",
-  getHttpErrorInfo: "/getHttpErrorInfo",
-  getBulkSymbolsState: "/getBulkSymbolsState",
-  setInterval: "/setInterval"
+  apis: ["https://api.aresprotocol.io"],
+  rootAPIURL: "https://api.aresprotocol.io",
+  auth: "/api/auth",
+  setWeight: "/api/setWeight",
+  getAresAll: "/api/getAresAll",
+  getPartyPrice: "/api/getPartyPrice", //btcusdt
+  getBulkCurrencyPrices: "/api/getBulkCurrencyPrices", // symbol=btc_eth_dot_link&currency=usdt
+  getReqConfig: "/api/getReqConfig",
+  getRequestInfoBySymbol: "/api/getRequestInfoBySymbol", // index=0&symbol=axsusdt
+  getHistoryPrices: "/api/getUpdatePriceHistory",
+  getPriceAll: "/api/getPriceAll",
+  getUpdatePriceHeartbeat: "/api/getUpdatePriceHeartbeat",
+  getHttpErrorInfo: "/api/getHttpErrorInfo",
+  getBulkSymbolsState: "/api/getBulkSymbolsState",
+  setInterval: "/api/setInterval"
 };
 
 export default Config;
--- src/views/Header.js
+++ src/views/Header.js
@@ -1,5 +1,59 @@
+import {Button, Input, Popover, message} from "antd";
+import config from "../Config";
+import {useState} from "react";
+import Config from "../Config";
 
 const Header = (props) => {
+  const [popoverVisible, setPopoverVisible] = useState(false);
+  const [newAddress, setNewAddress] = useState("");
+
+  const addNewAddress = async () => {
+    if (!newAddress.startsWith("https://")) {
+      message.error('address error, please start with http:// or https://');
+      return;
+    }
+    fetch(newAddress + Config.getAresAll, {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        source: "datafeed",
+      },
+    }).then(async res => {
+      const result = await res.json();
+      if (result && result.data) {
+        config.apis.push(newAddress);
+        setNewAddress("");
+        message.success('add new address success');
+      } else {
+        message.error('new address request data failed');
+      }
+    }).catch( error => {
+      message.error('new address request data failed');
+    })
+
+  };
+
+  const popoverContent = (
+      <div className="popoverContent">
+        {
+          config.apis.map(item => {
+            return <div className="popoverContentItem" key={item}>
+              <Button disabled={item === config.rootAPIURL} onClick={e => {
+                config.rootAPIURL = e.target.innerText;
+                setPopoverVisible(!popoverContent);
+              }}>
+                {item}
+              </Button>
+            </div>
+          })
+        }
+        <div style={{display: "flex", flexDirection: "column", rowGap: "5px", marginTop: "20px"}}>
+          <Input value={newAddress} placeholder="please input new data source address"  onChange={ e => setNewAddress(e.target.value)}/>
+          <Button type="primary" onClick={addNewAddress}>Add Source</Button>
+        </div>
+      </div>
+  );
+
   return (
     <header className="App-header">
       <div className="headerLayout">
@@ -10,6 +64,18 @@ const Header = (props) => {
           <span className="homeButton" onClick={() => {
             window.location = "/"
           }}>Home</span>
+          <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+
+          <Popover
+              content={popoverContent}
+              trigger="click"
+              visible={popoverVisible}
+              onVisibleChange={(visible) => {setPopoverVisible(visible)}}
+          >
+            <span className="switchButton" onClick={() => setPopoverVisible(true)}>
+              Switch Source
+            </span>
+          </Popover>
           <span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
           <select className="selectWithoutBorder">
             <option>English</option>
